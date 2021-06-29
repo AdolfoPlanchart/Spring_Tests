2572af28c88483b7e10adb4da6bd667c1b9e738f
Adolfo Planchart <47159444+AdolfoPlanchart@users.noreply.github.com>
2021-06-29 14:03:35 -0500

    Send data from controller to view.

--- Spring_Tests/src/main/java/com/planchart/tests/controller/FirstController.java
+++ Spring_Tests/src/main/java/com/planchart/tests/controller/FirstController.java
@@ -1,22 +1,30 @@
 package com.planchart.tests.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.planchart.tests.model.User;
 
 @Controller
 @RequestMapping("/")
 public class FirstController {
 
 	@GetMapping("/MethodOne")
-	public String MethodOne() {
+	public String MethodOne(Model model) {
+		model.addAttribute("user", new User("Adolfo","Planchart","aplanchart27@gmail.com"));
 		return "view_one";
 	}
 	
 	@GetMapping("/MethodTwo")
 	public ModelAndView MethodTwo() {
-		return new ModelAndView("view_two");
+		ModelAndView mav = new ModelAndView("view_two");
+		
+		mav.addObject("user", new User("Isabella","Planchart","isabellaplanchart@gmail.com"));
+		
+		return mav;
 	}
 	
 }
